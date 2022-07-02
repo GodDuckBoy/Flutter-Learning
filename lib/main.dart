98b55a14fba45f77c20b0393285f93b7b30845df
GodDuckBoy <a-ten2543@hotmail.com>
2022-07-02 14:03:11 +0700

    Flutter-Learning

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("StartApp Generator"),
-          actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.list))
-          ],),
-        body: Center(child: RandomWords()),
-      ),
+      home: RandomWords();
     );
   }
-}
-
-void _pushSaved() {
-  Navigator.push(MaterialPageRoute<void>)(
-    builder: (context) {
-      return Scaffold(
-        appBar: AppBar(title: "Saved"),
-        body: Container()
-      )
-    }
-  )
 }
 
 class RandomWords extends StatefulWidget {
@@ -48,33 +30,51 @@ class _RandomWordsState extends State<RandomWords> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, i) {
-      if (i.isOdd) return const Divider();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("StartApp Generator"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.list))
+        ],),
+      body:  ListView.builder(itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
 
-      final index = i ~/ 2;
-      if (index >= _suggestions.length) {
-        _suggestions.addAll(generateWordPairs().take(10));
-      }
-      final alreadySaved = _saved.contains(_suggestions[index]);
-      return ListTile(title: Text(
-        _suggestions[index].asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,),
-        onTap: () {
-          setState(() {
-            if (alreadySaved) {
-              _saved.remove(_suggestions[index]);
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        final alreadySaved = _saved.contains(_suggestions[index]);
+        return ListTile(title: Text(
+          _suggestions[index].asPascalCase,
+          style: _biggerFont,
+        ),
+          trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,),
+          onTap: () {
+            setState(() {
+              if (alreadySaved) {
+                _saved.remove(_suggestions[index]);
+              }
+              else {
+                _saved.add(_suggestions[index]);
+              }
             }
-            else {
-              _saved.add(_suggestions[index]);
-            }
-          }
+            );
+          },
+        );
+      }),
+    );
+
+  }
+  void _pushSaved() {
+    Navigator.push(MaterialPageRoute<void>)(
+        builder: (context) {
+          return Scaffold(
+              appBar: AppBar(title: "Saved"),
+              body: Container()
           );
-        },
-      );
-    });
+        }
+    );
   }
 }
 
